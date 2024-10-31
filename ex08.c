32120d577eec4578cd85d11b3ee6970cc9bb4132
KeremErkutf <240543013@firat.edu.tr>
2024-10-31 14:40:40 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
-int main(void){
+
+int main(void) {
     int array[10];
     int i;
     int sum = 0;
     float average;
 
-    for(i=0;i<10;i++){
-        printf("Enter the number: ");
-        scanf("%d",&array[i]);
+    for (i = 0; i < 10; i++) {
+        int check_int;
+
+        do {
+            printf("Enter the number: ");
+            check_int = scanf("%d", &array[i]);
+
+            if (check_int != 1) {
+                printf("Invalid value type. Please enter an integer.\n");
+                while (getchar() != '\n');  
+            }
+        } while (check_int != 1);  
+
         sum += array[i];
     }
-    average =(float) sum / 10;
 
-    printf("the number which you entered: \n");
-    for(i=0;i<10;i++){
+    average = (float) sum / 10;
+
+    printf("The numbers you entered:\n");
+    for (i = 0; i < 10; i++) {
         printf("%d, ", array[i]);
     }
     printf("\n");
-    printf("the sum is %d\n",sum);
-    printf("the average of the number is %.1f\n",average);
-    
+
+    printf("The sum is %d\n", sum);
+    printf("The average of the numbers is %.1f\n", average);    
 }
