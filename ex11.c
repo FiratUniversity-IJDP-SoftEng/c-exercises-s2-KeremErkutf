8d6accb015829716013f06beb59c2cce3bb22096
KeremErkutf <240543013@firat.edu.tr>
2024-11-01 23:37:26 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main(void){
+  int i;
+  int j;
 
+  for(i=0;i<=4;i++){
+    for(j=0;j<=4;j++){
+      if(i == j || i + j == 4){
+        printf("*");
+      }
+      else{
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
